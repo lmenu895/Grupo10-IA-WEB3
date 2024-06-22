f0b0d93c590110f94d1c3da5b059e556306488b4
Alan <arganarazalan603@gmail.com>
2024-06-22 19:37:41 -0300

    GALERIA MEMES

---
+++ ReconocimientoEmocionesIA/ReconocimientoEmocionesIA_Web/Controllers/GaleriaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ReconocimientoEmocionesIA_Entidades.EF;
+
+public class GaleriaController : Controller
+{
+    private readonly IGaleriaService _galeriaService;
+
+    public GaleriaController(IGaleriaService galeriaService)
+    {
+        _galeriaService = galeriaService;
+    }
+
+    public IActionResult Index()
+    {
+        List<MemeImage> memes = _galeriaService.ObtenerMemes();
+        return View(memes);
+    }
+}
