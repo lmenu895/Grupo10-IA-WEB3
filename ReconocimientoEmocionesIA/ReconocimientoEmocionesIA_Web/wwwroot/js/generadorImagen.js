2608afa84e73d8e1f948fa0b5e927d5cd1787685
Willams Meneses <menesestapia@gmail.com>
2024-06-23 19:30:32 -0300

    se agrega el maquetado sin mostrar el logo y alineacion del nav

--- ReconocimientoEmocionesIA/ReconocimientoEmocionesIA_Web/wwwroot/js/generadorImagen.js
+++ ReconocimientoEmocionesIA/ReconocimientoEmocionesIA_Web/wwwroot/js/generadorImagen.js
@@ -1,7 +1,7 @@
 ﻿const buttonImage = document.getElementById('buttonImage');
 const form = document.getElementById('uploadForm');
 
-buttonImage.addEventListener('click', (event) => {
+/*buttonImage.addEventListener('click', (event) => {
     event.preventDefault();
     const formData = new FormData(form);
 
@@ -12,4 +12,18 @@ buttonImage.addEventListener('click', (event) => {
 
     spinner.style.display = "block";
     realizarAccionFetch('/Home/GenerarImg', formData);
+});*/
+const fileInput = document.getElementById('imageUpload');
+
+// Agregar evento change al input file
+fileInput.addEventListener('change', function() {
+    // Verificar si se seleccionó un archivo
+    if (fileInput.files.length > 0) {
+        const formData = new FormData();
+        formData.append('imagenData', fileInput.files[0]); // Agregar el archivo al FormData
+
+        spinner.style.display = "block"; // Mostrar el spinner
+
+        realizarAccionFetch('/Home/GenerarImg', formData);
+    }
 });
